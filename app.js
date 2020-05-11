e318ff3e53d5caa39b50adcf532ccb08309dfcf5
meg <megan.daneika@gmail.com>
2020-05-11 15:54:59 +0100

    Add remove single task function

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ loadEventListeners();
 function loadEventListeners() {
   // Add task event
   form.addEventListener("submit", addTask);
+  // Remove task event
+  taskList.addEventListener("click", removeTask);
 }
 
 // Add Task
@@ -23,6 +25,7 @@ function addTask(e) {
   if (taskInput.value === "") {
     alert("add a task");
   }
+
   // Create li element
   const li = document.createElement("li");
   // Add class
@@ -30,19 +33,27 @@ function addTask(e) {
   // Create text node and append to li
   li.appendChild(document.createTextNode(taskInput.value));
   // Create new link element
-const link = document.createElement('a');
-//Add class
-link.className = "delete-item secondary-content";
-//Add icon html
-link.innerHTML = "<i class='fa fa-remove'></i>";
-// Append the link to li
-li.appendChild(link);
+  const link = document.createElement("a");
+  //Add class
+  link.className = "delete-item secondary-content";
+  //Add icon html
+  link.innerHTML = "<i class='fa fa-remove'></i>";
+  // Append the link to li
+  li.appendChild(link);
+  // Append li to ul
+  taskList.appendChild(li);
 
-// Append li to ul
-taskList.appendChild(li)
-
-// Clear input
-taskInput.value = "";
+  // Clear input
+  taskInput.value = "";
 
   e.preventDefault();
+}
+
+// Remove Task
+function removeTask(e) {
+  if (e.target.parentElement.classList.contains("delete-item")) {
+    if (confirm("Are you sure?")) {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
 }
